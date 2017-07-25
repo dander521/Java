6e71df510cb693bf27e2d0907746fbdf073715b8
程荣刚 <rong-gang.cheng@utsoft.cn>
2017-07-25 16:45:08 +0800

    添加最新工程

---
+++ src/main/java/Chapter1/BasicSyntax.java
@@ -0,0 +1,26 @@
+package Chapter1;
+
+class Direction {
+    enum DirectionArrow{
+        East,
+        West,
+        North,
+        Sourth
+    }
+    DirectionArrow arrow;
+}
+
+public class BasicSyntax {
+    public static void main(String[] args) {
+        /*
+        *   多行注释
+        *   多行注释
+        */
+        System.out.println("Hello World!");
+        // 单行注释
+        /* 单行注释 */
+        Direction direction = new Direction();
+        direction.arrow = Direction.DirectionArrow.West;
+        System.out.println(direction.arrow);
+    }
+}
