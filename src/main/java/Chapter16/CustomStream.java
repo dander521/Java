57276b611779a6e2c2a4d46b5ddc80828c652d78
程荣刚 <rong-gang.cheng@utsoft.cn>
2017-07-27 13:25:47 +0800

    添加控制台输入输出

---
+++ src/main/java/Chapter16/CustomStream.java
@@ -0,0 +1,37 @@
+package Chapter16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CustomStream {
+
+    public static void main(String args[]) throws IOException
+    {
+//        char c;
+//        // 使用 System.in 创建 BufferedReader
+//        BufferedReader br = new BufferedReader(new
+//                InputStreamReader(System.in));
+//        System.out.println("输入字符, 按下 'q' 键退出。");
+//        // 读取字符
+//        do {
+//            c = (char) br.read();
+//            System.out.println(c);
+//        } while(c != 'q');
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new
+                InputStreamReader(System.in));
+        String str;
+        System.out.println("Enter lines of text.");
+        System.out.println("Enter 'end' to quit.");
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while(!str.equals("end"));
+
+        int b;
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
