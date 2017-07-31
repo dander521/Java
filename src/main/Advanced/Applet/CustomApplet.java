0c16051a14c52979439ac90e5a78c7f219f936f4
程荣刚 <rong-gang.cheng@utsoft.cn>
2017-07-31 15:53:02 +0800

    添加数据库读取操作

--- src/main/Advanced/Applet/CustomApplet.java
+++ src/main/Advanced/Applet/CustomApplet.java
@@ -1,6 +1,7 @@
 package Applet;
 
 import java.net.InetAddress;
+import java.sql.*;
 
 public class CustomApplet {
     /*
@@ -17,11 +18,74 @@ public class CustomApplet {
     paint: 该方法在 start() 方法之后立即被调用，或者在 Applet 需要重绘在浏览器的时候调用。paint() 方法实际上继承于 java.awt。*/
 
     public static void main(String[] args)
-            throws Exception {
-        InetAddress addr = InetAddress.getLocalHost();
-        String hostname;
-        System.out.println("Local HostAddress: "+ addr.getHostAddress());
-                hostname = addr.getHostName();
-        System.out.println("Local host name: "+hostname);
+    {
+        //声明Connection对象
+        Connection con;
+        //驱动程序名
+        String driver = "com.mysql.jdbc.Driver";
+        //URL指向要访问的数据库名login
+        String url = "jdbc:mysql://localhost:3306/RUNOOB";
+        //MySQL配置时的用户名
+        String user = "root";
+        //MySQL配置时的密码
+        String password = "asd";
+        //遍历查询结果集
+        try
+        {
+            //加载驱动程序
+            Class.forName(driver);
+            //1.getConnection()方法，连接MySQL数据库！！
+            con = DriverManager.getConnection(url,user,password);
+            if(!con.isClosed())
+                System.out.println("Succeeded connecting to the Database!");
+            //2.创建statement类对象，用来执行SQL语句！！
+            Statement statement = con.createStatement();
+            //要执行的SQL语句
+            String sql = "select * from website";    //从建立的login数据库的login——message表单读取数据
+            //3.ResultSet类，用来存放获取的结果集！！
+            ResultSet rs = statement.executeQuery(sql);
+            System.out.println("-----------------");
+            System.out.println("执行结果如下所示:");
+            System.out.println("-----------------");
+            System.out.println(" 姓名" + "\t" + " 密码");
+            System.out.println("-----------------");
+            String name = null;
+            String login_password = null;
+            while(rs.next())
+            {
+                //获取stuname这列数据
+                name = rs.getString("name");
+                //获取stuid这列数据
+                login_password = rs.getString("url");
+                //首先使用ISO-8859-1字符集将name解码为字节序列并将结果存储新的字节数组中。
+                //然后使用GB2312字符集解码指定的字节数组。
+                name = new String(name.getBytes("utf8"),"utf8");
+                //输出结果
+                System.out.println(name + "\t" + login_password);
+            }
+            rs.close();
+            con.close();
+        }
+        catch(ClassNotFoundException e)
+        {
+            //数据库驱动类异常处理
+            System.out.println("Sorry,can`t find the Driver!");
+            e.printStackTrace();
+        }
+        catch(SQLException e)
+        {
+            //数据库连接失败异常处理
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("数据库数据成功获取！！");
+        }
     }
+
 }
